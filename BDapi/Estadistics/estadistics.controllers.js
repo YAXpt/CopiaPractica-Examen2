5b12aa12a3517270504ac2fa9d68c423233d4a57
YAXpt <aylachic@gmail.com>
2025-01-15 08:45:21 +0100

    2

--- BDapi/Estadistics/estadistics.controllers.js
+++ BDapi/Estadistics/estadistics.controllers.js
@@ -1,24 +1,40 @@
+/* Cada evento es una nueva estadistica, cada estadistica/evento viene de un user y de una session,
+ no siempre el mismo user tendrá una misma session, cada evento tiene unas caracteristicas */
+
+
 import { estadisticsModel } from './estadistics.model.js';
 
 export async function createEstadistics(req, res) { //POST 
     const { sessionId, userId, llocEvent, tipusEvent } = req.body;
-    const existingSessionId = await estadisticsModel.findOne({ sessionId: sessionId });
-    if (existingSessionId) {
-        return res.status(400).json({ message: "Already generated this sessionId" });
-    }
-
+    
     const newEstadistics = new estadisticsModel({
-        sessionId: String,
-        userId: String,
-        llocEvent: String,
-        tipusEvent: String
+        sessionId: sessionId,
+        userId: userId,
+        llocEvent: llocEvent,
+        tipusEvent: tipusEvent
     });
 
     await newEstadistics.save();
     res.json(newEstadistics);
 }
 
-export async function handleGetEstadistics(req, res) { //GET
+
+
+export async function handleEstadistics(req, res) { //GET
     const { sessionId } = req.body;
-    const foundUser = await userModel.findOne({ email });
+     const estadistics = await estadisticsModel.find({sessionId});
+        res.json({ results: estadistics });
+
+    res.status(200).json(foundSession);
+}
+
+export async function handleLastEstadistics(req, res) { //GET
+    const { sessionId } = req.body;
+    const foundSession = await estadisticsModel.findOne({sessionId});
+    if (!foundSession) {
+        return res.status(404).json({ message: "This session hasn't been created" });
+    }
+
+
+    
 }
